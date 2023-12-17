72e16cd58291a388b49e178f60ff516c94361881
Ruben Figge <91072139+rfigge24@users.noreply.github.com>
2023-12-17 12:34:09 +0100

    Update sentence_generator.py
    lil changes

--- woord voorspeller/sentence_generator.py
+++ woord voorspeller/sentence_generator.py
@@ -21,14 +21,17 @@ class Predicter:
                     self.n_min_one2ngram_dict[ngram[:-1]].append(ngram)
 
 
-#Testtttttssssssssssss: 
-predict2 = Predicter("./models/2-sprookjes-cleaned.txt")
-predict3 = Predicter("./models/3-sprookjes-cleaned.txt")
-predict4 = Predicter("./models/4-sprookjes-cleaned.txt")
+#-----------------------------------------------------------------
+# Models currently in use:
+#-----------------------------------------------------------------
+
+predict2 = Predicter("./models/2-Het-Boek-cleaned.txt")
+predict3 = Predicter("./models/3-Het-Boek-cleaned.txt")
+predict4 = Predicter("./models/4-Het-Boek-cleaned.txt")
 
 
 
-def genfirstword():
+def gen_first_word():
 
     startwords = predict2.n_min_one2ngram_dict[('<s>',)]
     startwordspobability = tuple([predict2.ngram_probability_table[ngram] for ngram in startwords])
@@ -36,7 +39,7 @@ def genfirstword():
     chosenngram = random.choices(startwords, weights=startwordspobability)[0]
     return chosenngram
 
-def gensecondword(old_ngram):
+def gen_second_word(old_ngram):
 
     startwords = predict3.n_min_one2ngram_dict[old_ngram]
     startwordspobability = tuple([predict3.ngram_probability_table[ngram] for ngram in startwords])
@@ -44,7 +47,7 @@ def gensecondword(old_ngram):
     chosenngram = random.choices(startwords, weights=startwordspobability)[0]
     return chosenngram
 
-def genthirtword(old_ngram):
+def gen_thirt_word(old_ngram):
 
     startwords = predict4.n_min_one2ngram_dict[old_ngram]
     startwordspobability = tuple([predict4.ngram_probability_table[ngram] for ngram in startwords])
@@ -55,19 +58,19 @@ def genthirtword(old_ngram):
 
 #-----------------------------------------------------------------
 
-#De te gebruiken functies in de console aan de rechter kant
+# Usable functions from the console to generate sentences:
 
-#-------------------------------------------------------------------
+#-----------------------------------------------------------------
 def gensentence_3grams():
     old_ngram = None
     sentence = ""
     
-    firstwords = genfirstword()
+    firstwords = gen_first_word()
     sentence += " ".join(firstwords)
     old_ngram = firstwords
     
     while old_ngram[-1] != "</s>":
-        nextwords = gensecondword(old_ngram)
+        nextwords = gen_second_word(old_ngram)
         sentence += " " + nextwords[-1]
         old_ngram = nextwords[1:]
         
@@ -77,16 +80,16 @@ def gensentence_4grams():
     old_ngram = None
     sentence = ""
     
-    firstwords = genfirstword()
+    firstwords = gen_first_word()
     sentence += " ".join(firstwords)
     old_ngram = firstwords
     
-    secondwords = gensecondword(old_ngram)
+    secondwords = gen_second_word(old_ngram)
     sentence += " " + secondwords[-1]
     old_ngram = secondwords
     
     while old_ngram[-1] != "</s>":
-        nextwords = genthirtword(old_ngram)
+        nextwords = gen_thirt_word(old_ngram)
         sentence += " " + nextwords[-1]
         old_ngram = nextwords[1:]
         
